8d708cb1f18ce9cfaf1ad6f1e44f7ec47fccfb5e
Dessemy <Dessmy@proton.me>
2026-08-26 05:28:31 +0700

    Update config.def.h

--- dwl/slstatus/config.def.h
+++ dwl/slstatus/config.def.h
@@ -12,7 +12,10 @@ static const char unknown_str[] = "n/a";
 /* see components/ for all available functions and their arguments */
 static const struct arg args[] = {
 	/* function          format          argument */
-	{ battery_perc,        "BAT %s%%",             "BAT0" },
-	{ battery_state,       " (%s)",                "BAT0" },
-	{ datetime,            "   %s   ",             "%a, %b %e   %I:%M:%S %p" },
+	{ run_command,   "%s",
+	  "b=$(cat /sys/class/power_supply/BAT0/capacity 2>/dev/null); "
+	  "s=$(cat /sys/class/power_supply/BAT0/status 2>/dev/null); "
+	  "c=32; [ \"$b\" -lt 60 ] 2>/dev/null && c=33; [ \"$b\" -lt 20 ] 2>/dev/null && c=31; "
+	  "printf \"\033[36mBAT \033[%sm%s%%\033[0m (%s)\" \"$c\" \"$b\" \"$s\"" },
+	{ datetime,      "\033[35m   %s   \033[0m",                              "%a, %b %e   %I:%M:%S %p" },
 };
