d5ece103afb9173ba3014bee1adfe24fb69cfa27
Dessemy <Dessmy@proton.me>
2026-09-04 16:47:48 +0700

    Add files via upload

--- dwl/config.def.h
+++ dwl/config.def.h
@@ -125,6 +125,7 @@ static const Key keys[] = {
 	{ MODKEY|WLR_MODIFIER_CTRL,            XKB_KEY_p,              spawn,            SHCMD("$HOME/.config/scripts/pwrmode " WMENU) },
 	{ MODKEY|WLR_MODIFIER_CTRL,            XKB_KEY_r,              spawn,            SHCMD("$HOME/.config/scripts/randr " WMENU) },
 	{ MODKEY,                              XKB_KEY_Escape,         spawn,            SHCMD("waylock -init-color 0x1a1b26 -input-color 0x7aa2f7 -input-alt-color 0xad8ee6 -fail-color 0xf7768e") },
+	{ MODKEY,                              XKB_KEY_p,              spawn,            SHCMD("hyprpicker -a && notify-send 'Hyprpicker' 'Copied to clipboard'") },
 
 	{ MODKEY,                              XKB_KEY_F6,             spawn,            SHCMD("brightnessctl set +5% && val=$(brightnessctl -m | cut -d, -f4 | tr -d %) && notify-send -a brightness -h string:x-canonical-private-synchronous:brightness -h int:value:$val -h string:bgcolor:'#e0af68' -h string:fgcolor:'#e0af68' -h string:frcolor:'#e0af68' -t 1500 ' ' ' '") },
 	{ MODKEY,                              XKB_KEY_F5,             spawn,            SHCMD("brightnessctl set 5%- && val=$(brightnessctl -m | cut -d, -f4 | tr -d %) && notify-send -a brightness -h string:x-canonical-private-synchronous:brightness -h int:value:$val -h string:bgcolor:'#e0af68' -h string:fgcolor:'#e0af68' -h string:frcolor:'#e0af68' -t 1500 ' ' ' '") },
